ad0948044a94f8f3166b472e25e7599b9edeea7a
Dmitry Shachnev <mitya57@gmail.com>
2011-05-22 12:59:45 +0400

    Added Czech translation
    Many thanks to w4rr10r!

---
+++ retext_cs.ts
@@ -0,0 +1,331 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>LogPassDialog</name>
+    <message>
+        <location filename="retext.py" line="107"/>
+        <source>Enter your Google account data</source>
+        <translation>Zadejte své přihlašovací údaje k účtu Google</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="116"/>
+        <source>Username</source>
+        <translation>Uživatelské jméno</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="117"/>
+        <source>Password</source>
+        <translation>Heslo</translation>
+    </message>
+</context>
+<context>
+    <name>ReTextWindow</name>
+    <message>
+        <location filename="retext.py" line="702"/>
+        <source>New document</source>
+        <translation>Nový dokument</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="177"/>
+        <source>File toolbar</source>
+        <translation>Nástrojová lišta Soubor</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="179"/>
+        <source>Edit toolbar</source>
+        <translation>Nástrojová lišta Úpravy</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="181"/>
+        <source>New</source>
+        <translation>Nový</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="185"/>
+        <source>Open</source>
+        <translation>Otevřít</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="189"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="194"/>
+        <source>Save as</source>
+        <translation>Uložit jako</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="197"/>
+        <source>Print</source>
+        <translation>Tisknout</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="201"/>
+        <source>Print preview</source>
+        <translation>Náhled tisku</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="203"/>
+        <source>View HTML code</source>
+        <translation>Zobrazit HTML kód</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="205"/>
+        <source>Change default font</source>
+        <translation>Změnit výchozí písmo</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="207"/>
+        <source>Preview</source>
+        <translation>Náhled</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="219"/>
+        <source>Live preview</source>
+        <translation>Průběžný náhled</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="231"/>
+        <source>Other formats</source>
+        <translation>Jiné formáty</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="237"/>
+        <source>Quit</source>
+        <translation>Ukončit</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="241"/>
+        <source>Undo</source>
+        <translation>Zpět</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="243"/>
+        <source>Redo</source>
+        <translation>Znovu</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="247"/>
+        <source>Copy</source>
+        <translation>Kopírovat</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="250"/>
+        <source>Cut</source>
+        <translation>Vyjmout</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="253"/>
+        <source>Paste</source>
+        <translation>Vložit</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="262"/>
+        <source>Plain text</source>
+        <translation>Prostý text</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="524"/>
+        <source>Open recent</source>
+        <translation>Otevřít nedávné</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="268"/>
+        <source>Generate webpages</source>
+        <translation>Generovat webové stránky</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="270"/>
+        <source>Show</source>
+        <translation>Zobrazit</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="794"/>
+        <source>About %1</source>
+        <translation>O %1</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="275"/>
+        <source>About Qt</source>
+        <translation>O Qt</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="277"/>
+        <source>Markdown syntax examples</source>
+        <translation>Ukázky syntaxe Markdown</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="280"/>
+        <source>Save to Google Docs</source>
+        <translation>Uložit na Google Docs</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="286"/>
+        <source>Tags</source>
+        <translation>Značky</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="290"/>
+        <source>Symbols</source>
+        <translation>Entity</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="296"/>
+        <source>File</source>
+        <translation>Soubor</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="297"/>
+        <source>Edit</source>
+        <translation>Upravit</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="298"/>
+        <source>Help</source>
+        <translation>Nápověda</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="302"/>
+        <source>Directory</source>
+        <translation>Složka</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="310"/>
+        <source>Export</source>
+        <translation>Exportovat</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="364"/>
+        <source>Markdown module not found!</source>
+        <translation>Modul Markdown nebyl nalezen!</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="364"/>
+        <source>Only HTML formatting will be available.</source>
+        <translation>Bude dostupné pouze HTML formátování.</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="497"/>
+        <source>Please, save the file somewhere.</source>
+        <translation>Soubor, prosím, někam uložte.</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="491"/>
+        <source>Webpages saved in &lt;code&gt;html&lt;/code&gt; directory.</source>
+        <translation>Webové stránky uloženy do složky &lt;code&gt;html&lt;/code&gt;.</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="529"/>
+        <source>Open file</source>
+        <translation>Otevřít soubor</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="529"/>
+        <source>Supported files (*.re *.md *.txt *.html *.htm)</source>
+        <translation>Podporované soubory (*.re *.md *.txt *.html *.htm)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="529"/>
+        <source>All files (*)</source>
+        <translation>Všechny soubory (*)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="573"/>
+        <source>Plain text (*.txt)</source>
+        <translation>Prostý text (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="576"/>
+        <source>ReText files (*.re *.md *.txt)</source>
+        <translation>Soubory ReText (*.re *.md *.txt)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="640"/>
+        <source>Save file</source>
+        <translation>Uložit soubor</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="590"/>
+        <source>Cannot save to file since it is read-only!</source>
+        <translation>Soubor nelze uložit, protože je pouze pro čtení!</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="631"/>
+        <source>Export document to ODT</source>
+        <translation>Exportovat dokument do ODT</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="631"/>
+        <source>OpenDocument text files (*.odt)</source>
+        <translation>Textové soubory OpenDocument (*.odt)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="640"/>
+        <source>HTML files (*.html *.htm)</source>
+        <translation>Soubory HTML (*.html *.htm)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="645"/>
+        <source>Export document to PDF</source>
+        <translation>Exportovat dokument do PDF</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="645"/>
+        <source>PDF files (*.pdf)</source>
+        <translation>Soubory PDF (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="660"/>
+        <source>Print document</source>
+        <translation>Tisknout dokument</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="676"/>
+        <source>This function is not available in Plain text mode!</source>
+        <translation>Tato funkce není k dispozici v režimu Prostý text!</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="682"/>
+        <source>Select type</source>
+        <translation>Vyberte typ</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="684"/>
+        <source>Export document</source>
+        <translation>Exportovat dokument</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="717"/>
+        <source>Incorrect user name or password!</source>
+        <translation>Nesprávné uživatelské jméno nebo heslo!</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="761"/>
+        <source>The document has been modified.
+Do you want to save your changes?</source>
+        <translation>Dokument byl změněn. Chcete uložit své změny?</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="782"/>
+        <source>HTML code</source>
+        <translation>HTML kód</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="794"/>
+        <source>This is &lt;b&gt;%1&lt;/b&gt;, version %2&lt;br&gt;Author: Dmitry Shachnev, 2011</source>
+        <translation>Toto je &lt;b&gt;%1&lt;/b&gt;, verze %2&lt;br&gt;Autor: Dmitry Shachnev, 2011</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="794"/>
+        <source>Website: &lt;a href=&quot;http://sourceforge.net/p/retext/&quot;&gt;sf.net/p/retext&lt;/a&gt;</source>
+        <translation>Webové stránky: &lt;a href=&quot;http://sourceforge.net/p/retext/&quot;&gt;sf.net/p/retext&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="retext.py" line="794"/>
+        <source>Markdown syntax documentation: &lt;a href=&quot;http://daringfireball.net/projects/markdown/syntax&quot;&gt;daringfireball.net/projects/markdown/syntax&lt;/a&gt;</source>
+        <translation>Dokumentace Markdown syntaxe: &lt;a href=&quot;http://daringfireball.net/projects/markdown/syntax&quot;&gt;daringfireball.net/projects/markdown/syntax&lt;/a&gt;</translation>
+    </message>
+</context>
+</TS>
