8e7698c1abad3f6af6379d8ce15f715ea985e793
Dmitry Shachnev <mitya57@gmail.com>
2012-01-05 17:30:55 +0400

    Rename Chinese translations to get them working

--- locale/retext_zh_TW.ts
+++ locale/retext_zh_TW.ts
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="zh_CN" sourcelanguage="">
+<!DOCTYPE TS><TS version="2.0" language="zh_TW" sourcelanguage="">
 <context>
     <name>LogPassDialog</name>
     <message>
