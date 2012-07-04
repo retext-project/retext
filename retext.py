ac683a13ceff5ca391af7825d58ad36c9cfdc049
Dmitry Shachnev <mitya57@gmail.com>
2012-07-04 23:17:07 +0400

    openFile: build the list of supported extensions from pydocuments data

--- retext.py
+++ retext.py
@@ -997,8 +997,12 @@ class ReTextWindow(QMainWindow):
 		return extension
 	
 	def openFile(self):
+		supportedExtensions = ['.txt']
+		for markup in documents.known_markups:
+			supportedExtensions = supportedExtensions + list(markup.file_extensions)
+		fileFilter = ' (' + str.join(' ', ['*'+ext for ext in supportedExtensions]) + ');;'
 		fileNames = QFileDialog.getOpenFileNames(self, self.tr("Select one or several files to open"), "",
-		self.tr("Supported files")+" (*.re *.md *.markdown *.mdown *.mkd *.mkdn *.rst *.rest *.txt);;"+self.tr("All files (*)"))
+		self.tr("Supported files") + fileFilter + self.tr("All files (*)"))
 		for fileName in fileNames:
 			self.openFileWrapper(fileName)
 	
