4c14cdac2aa3aa05f81450dba7fa20015bcbe53e
Dmitry Shachnev <mitya57@gmail.com>
2012-08-13 14:46:53 +0400

    editBoxMenuEvent: show suggestions if the incorrect word was selected

--- ReText/window.py
+++ ReText/window.py
@@ -385,9 +385,9 @@ class ReTextWindow(QMainWindow):
 		editBox = self.editBoxes[self.ind]
 		text = editBox.toPlainText()
 		dictionary = self.highlighters[self.ind].dictionary
-		oldcursor = editBox.textCursor()
-		if (dictionary is None) or (not text) or oldcursor.hasSelection():
+		if (dictionary is None) or not text:
 			return QTextEdit.contextMenuEvent(editBox, event)
+		oldcursor = editBox.textCursor()
 		cursor = editBox.cursorForPosition(event.pos())
 		pos = cursor.positionInBlock()
 		if pos == len(text): pos -= 1
@@ -398,9 +398,10 @@ class ReTextWindow(QMainWindow):
 			# For Python 2
 			curchar = text.at(pos)
 			isalpha = curchar.isLetter()
-		if not isalpha:
-			return QTextEdit.contextMenuEvent(editBox, event)
 		cursor.select(QTextCursor.WordUnderCursor)
+		if not isalpha or (oldcursor.hasSelection() and
+		oldcursor.selectedText() != cursor.selectedText()):
+			return QTextEdit.contextMenuEvent(editBox, event)
 		editBox.setTextCursor(cursor)
 		word = convertToUnicode(cursor.selectedText())
 		if not word or dictionary.check(word):
