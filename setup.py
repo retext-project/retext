b1f0fa3671620bfe344f4338501fbb07f2f9a9f8
Dmitry Shachnev <mitya57@gmail.com>
2012-09-17 15:35:34 +0400

    setup.py: build translations using lrelease

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.sdist import sdist
+from subprocess import check_call
+from glob import glob
 from ReText import app_version
+
+def build_translations():
+	print('running build_translations')
+	error = None
+	for ts_file in glob('locale/*.ts'):
+		try:
+			check_call(('lrelease', ts_file))
+		except Exception as e:
+			error = e
+	if error:
+		print(error)
+
+class retext_build(build):
+	def run(self):
+		build.run(self)
+		build_translations()
+
+class retext_sdist(sdist):
+	def run(self):
+		build_translations()
+		sdist.run(self)
 
 setup(name='ReText',
 	version=app_version,
@@ -11,5 +36,6 @@ setup(name='ReText',
 	url='http://retext.sourceforge.net/',
 	packages=['ReText'],
 	scripts=['retext.py', 'wpgen.py'],
+	cmdclass={'build': retext_build, 'sdist': retext_sdist},
 	license='GPL 2+'
 )
