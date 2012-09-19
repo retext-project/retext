c32d0591e0c07c338ee4adfea092fe6448d35015
Dmitry Shachnev <mitya57@gmail.com>
2012-09-19 19:15:45 +0400

    Revert "Don't import markups in __init__, so that the package can be built without it"
    This reverts commit cb18d22d12d36b5c2b3b9e3f75d953e89b10e018.

--- ReText/__init__.py
+++ ReText/__init__.py
@@ -2,6 +2,7 @@
 # Copyright: Dmitry Shachnev 2012
 # License: GNU GPL v2 or higher
 
+import markups
 from subprocess import Popen, PIPE
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -22,8 +23,8 @@ else:
 icon_path = "icons/"
 
 DOCTYPE_NONE = ''
-DOCTYPE_MARKDOWN = 'Markdown'
-DOCTYPE_REST = 'reStructuredText'
+DOCTYPE_MARKDOWN = markups.MarkdownMarkup.name
+DOCTYPE_REST = markups.ReStructuredTextMarkup.name
 DOCTYPE_HTML = 'html'
 
 try:
