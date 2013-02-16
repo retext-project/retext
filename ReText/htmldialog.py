c05ac0a5dbde487074253bf5ce7ed68bcb64f074
Dmitry Shachnev <mitya57@gmail.com>
2013-02-16 18:41:32 +0400

    htmldialog: make some variables local for lower memory consumption

--- ReText/htmldialog.py
+++ ReText/htmldialog.py
@@ -9,17 +9,17 @@ class HtmlDialog(QDialog):
 	def __init__(self, parent=None):
 		QDialog.__init__(self, parent)
 		self.resize(700, 600)
-		self.verticalLayout = QVBoxLayout(self)
+		verticalLayout = QVBoxLayout(self)
 		self.textEdit = QTextEdit(self)
 		self.textEdit.setReadOnly(True)
 		self.textEdit.setFont(monofont)
 		self.hl = ReTextHighlighter(self.textEdit.document())
 		self.hl.docType = DOCTYPE_HTML
-		self.verticalLayout.addWidget(self.textEdit)
-		self.buttonBox = QDialogButtonBox(self)
-		self.buttonBox.setStandardButtons(QDialogButtonBox.Close)
-		self.connect(self.buttonBox, SIGNAL("clicked(QAbstractButton*)"), self.doClose)
-		self.verticalLayout.addWidget(self.buttonBox)
+		verticalLayout.addWidget(self.textEdit)
+		buttonBox = QDialogButtonBox(self)
+		buttonBox.setStandardButtons(QDialogButtonBox.Close)
+		self.connect(buttonBox, SIGNAL("clicked(QAbstractButton*)"), self.doClose)
+		verticalLayout.addWidget(buttonBox)
 	
 	def doClose(self):
 		self.close()
