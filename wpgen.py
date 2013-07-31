ac55d499444007cd185991226f9f2060b6d40e01
Dmitry Shachnev <mitya57@gmail.com>
2013-07-31 15:54:36 +0400

    Bump years in copyright

--- wpgen.py
+++ wpgen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # ReText webpages generator
-# Copyright 2011-2012 Dmitry Shachnev
+# Copyright 2011-2013 Dmitry Shachnev
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
