52253cf3d3552e872a0a26833bc761deabc63371
Dmitry Shachnev <mitya57@gmail.com>
2015-09-16 10:20:11 +0300

    Add proper GPL headers to all files

--- ReText/window.py
+++ ReText/window.py
@@ -1,7 +1,18 @@
-# vim: noexpandtab:ts=4:sw=4
 # This file is part of ReText
-# Copyright: Dmitry Shachnev 2012-2015
-# License: GNU GPL v2 or higher
+# Copyright: 2012-2015 Dmitry Shachnev
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import markups
 import sys
--- tests/test_tablemode.py
+++ tests/test_tablemode.py
@@ -1,7 +1,18 @@
-# vim: noexpandtab:ts=4:sw=4
 # This file is part of ReText
-# Copyright: Maurice van der Pot 2014
-# License: GNU GPL v2 or higher
+# Copyright: 2014 Maurice van der Pot
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 from ReText import tablemode
