a6728a140080cb6f93303104ece89ac76a607b1a
Dmitry Shachnev <mitya57@gmail.com>
2015-12-20 17:07:23 +0300

    window: Fix ODT export

--- ReText/window.py
+++ ReText/window.py
@@ -837,7 +837,7 @@ class ReTextWindow(QMainWindow):
 		if not QFileInfo(fileName).suffix():
 			fileName += ".odt"
 		writer = QTextDocumentWriter(fileName)
-		writer.setFormat("odf")
+		writer.setFormat(b"odf")
 		writer.write(document)
 
 	def saveFileHtml(self):
