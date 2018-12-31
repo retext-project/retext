8c55ca4e4b7a6010b0a85f5b4b4470596d5e298a
Dmitry Shachnev <mitya57@gmail.com>
2018-12-31 14:42:09 +0300

    Add copyright header

--- ReText/tabledialog.py
+++ ReText/tabledialog.py
@@ -1,3 +1,19 @@
+# This file is part of ReText
+# Copyright: 2018 Changhee Kim, 2018 Dmitry Shachnev
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from markups import ReStructuredTextMarkup
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QGridLayout, QLabel, \
