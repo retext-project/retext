17fdce82bc14edfddb0d0ab5990ac85dc76f2687
Dmitry Shachnev <mitya57@gmail.com>
2018-12-31 13:44:03 +0300

    Remove self.configurators

--- ReText/table.py
+++ ReText/table.py
@@ -13,28 +13,27 @@ class TableWizardDialog(QDialog):
         buttonBox.accepted.connect(self.makeTable)
         buttonBox.rejected.connect(self.close)
 
-        self.configurators = {}
         layout = QGridLayout(self)
 
         label_row = QLabel('Row:', self)
         label_column = QLabel('Column:', self)
-        self.configurators['tableDefaultRowCount'] = QSpinBox(self)
-        self.configurators['tableDefaultColumnCount'] = QSpinBox(self)
+        self.rowsSpinBox = QSpinBox(self)
+        self.columnsSpinBox = QSpinBox(self)
 
-        self.configurators['tableDefaultRowCount'].setRange(1, 10)
-        self.configurators['tableDefaultColumnCount'].setRange(1, 10)
-        self.configurators['tableDefaultRowCount'].setValue(3)
-        self.configurators['tableDefaultColumnCount'].setValue(3)
+        self.rowsSpinBox.setRange(1, 10)
+        self.columnsSpinBox.setRange(1, 10)
+        self.rowsSpinBox.setValue(3)
+        self.columnsSpinBox.setValue(3)
 
         layout.addWidget(label_row, 0, 0)
-        layout.addWidget(self.configurators['tableDefaultRowCount'], 0, 1, Qt.AlignRight)
+        layout.addWidget(self.rowsSpinBox, 0, 1, Qt.AlignRight)
         layout.addWidget(label_column, 1, 0)
-        layout.addWidget(self.configurators['tableDefaultColumnCount'], 1, 1, Qt.AlignRight)
+        layout.addWidget(self.columnsSpinBox, 1, 1, Qt.AlignRight)
         layout.addWidget(buttonBox, 2, 0, 1, 2)
 
     def makeTable(self):
-        rowCount = self.configurators['tableDefaultRowCount'].value()
-        columnCount = self.configurators['tableDefaultColumnCount'].value()
+        rowCount = self.rowsSpinBox.value()
+        columnCount = self.columnsSpinBox.value()
 
         # Table column's name section (table Header)
         tableHeader = ''
