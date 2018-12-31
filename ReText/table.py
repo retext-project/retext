45099c94952702d6155d4f19894d02be0aa0746f
khe0613 <khe0616@cnu.ac.kr>
2018-12-31 15:03:02 +0900

    remove signal(line 8)

--- ReText/table.py
+++ ReText/table.py
@@ -5,8 +5,6 @@ from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTabWidget, \
 
 
 class ClickableLabel(QLabel):
-    clicked = pyqtSignal()
-
     def mousePressEvent(self, event):
         self.clicked.emit()
         super().mousePressEvent(event)
