d349f13addea6d2d6edb2e845b294c0667c5453f
Guo Yunhe <i@guoyunhe.me>
2020-02-25 14:12:43 +0200

    Enable HiDPI icons

--- ReText/__main__.py
+++ ReText/__main__.py
@@ -60,6 +60,7 @@ def main():
 
 	# Needed for Qt WebEngine on Windows
 	QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
+	QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps);
 	app = QApplication(sys.argv)
 	app.setOrganizationName("ReText project")
 	app.setApplicationName("ReText")
