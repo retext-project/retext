354821e537b8b2d98939d81a3940ae1fe2f3f1af
Dmitry Shachnev <mitya57@gmail.com>
2021-07-25 21:39:54 +0300

    Add new tests for the auto-save mode and reloading logic

--- tests/test_window.py
+++ tests/test_window.py
@@ -28,7 +28,9 @@ import warnings
 from markups.abstract import ConvertedMarkup
 
 from PyQt5.QtCore import pyqtSignal, QObject, Qt
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QTextCursor
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication, QMessageBox
 import ReText
 from ReText.window import ReTextWindow
 
@@ -58,7 +60,8 @@ class TestWindow(unittest.TestCase):
         self.readListFromSettingsMock = patch('ReText.window.readListFromSettings', return_value=[]).start()
         self.writeListToSettingsMock  = patch('ReText.window.writeListToSettings').start()
         self.globalSettingsMock       = patch('ReText.window.globalSettings', MagicMock(**ReText.configOptions)).start()
-        self.fileSystemWatcherMock    = patch('ReText.window.QFileSystemWatcher').start()
+        self.fileSystemWatcherPatcher = patch('ReText.window.QFileSystemWatcher')
+        self.fileSystemWatcherMock    = self.fileSystemWatcherPatcher.start()
         ReText.tab.globalSettings = self.globalSettingsMock
 
     def tearDown(self):
@@ -362,7 +365,78 @@ class TestWindow(unittest.TestCase):
         with suppress(PermissionError):
             os.remove(fileName)
 
+    def test_autoSave(self):
+        self.globalSettingsMock.autoSave = True
+        window = ReTextWindow()
+        window.autoSaveTimer.start(250)
+        fileName = tempfile.mkstemp(suffix='.mkd')[1]
+        with open(fileName, 'w', encoding='utf-8') as tempFile:
+            tempFile.write('first content')
+        window.openFileWrapper(fileName)
+
+        cursor = window.currentTab.editBox.textCursor()
+        cursor.select(QTextCursor.SelectionType.Document)
+        cursor.insertText('second content')
+        QTest.qWait(300)  # more than the timer interval
+        with open(fileName, 'r', encoding='utf-8') as tempFile:
+            self.assertEqual(tempFile.read(), 'second content')
+
+        window.closeTab(0)
+        with suppress(PermissionError):
+            os.remove(fileName)
+
+    @patch('ReText.window.QMessageBox.exec', return_value=None)
+    def test_reloadFileNotModified(self, messageBoxExecMock):
+        self.fileSystemWatcherPatcher.stop()
+        window = ReTextWindow()
+        fileName = tempfile.mkstemp(suffix='.mkd')[1]
+        with open(fileName, 'w', encoding='utf-8') as tempFile:
+            tempFile.write('first content')
+        window.openFileWrapper(fileName)
+        self.assertEqual(window.fileSystemWatcher.files(), [fileName.replace('\\', '/')])
+        editBox = window.currentTab.editBox
+        self.assertEqual(editBox.toPlainText(), 'first content')
+        self.assertFalse(editBox.document().isModified())
+        app.processEvents()
+
+        with open(fileName, 'w', encoding='utf-8') as tempFile:
+            tempFile.write('modified externally')
+        QTest.qWait(100)
+        self.assertEqual(editBox.toPlainText(), 'modified externally')
+        self.assertFalse(window.currentTab.forceDisableAutoSave)
+
+        window.closeTab(0)
+        self.assertEqual(window.fileSystemWatcher.files(), [])
+        with suppress(PermissionError):
+            os.remove(fileName)
+
+    @patch('ReText.window.QMessageBox.warning', return_value=QMessageBox.StandardButton.Discard)
+    @patch('ReText.window.QMessageBox.exec', return_value=None)
+    def test_reloadFileModified(self, messageBoxExecMock, messageBoxWarningMock):
+        self.fileSystemWatcherPatcher.stop()
+        window = ReTextWindow()
+        fileName = tempfile.mkstemp(suffix='.mkd')[1]
+        with open(fileName, 'w', encoding='utf-8') as tempFile:
+            tempFile.write('first content')
+        window.openFileWrapper(fileName)
+        self.assertEqual(window.fileSystemWatcher.files(), [fileName.replace('\\', '/')])
+        editBox = window.currentTab.editBox
+        self.assertEqual(editBox.toPlainText(), 'first content')
+
+        editBox.textCursor().insertText('modified ')
+        app.processEvents()
+        self.assertTrue(editBox.document().isModified())
+
+        with open(fileName, 'w', encoding='utf-8') as tempFile:
+            tempFile.write('modified externally')
+        QTest.qWait(100)
+        self.assertEqual(editBox.toPlainText(), 'modified first content')
+        self.assertTrue(window.currentTab.forceDisableAutoSave)
+
+        window.closeTab(0)
+        self.assertEqual(window.fileSystemWatcher.files(), [])
+        with suppress(PermissionError):
+            os.remove(fileName)
 
 if __name__ == '__main__':
     unittest.main()
-
