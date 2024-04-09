6941d00871566b52c2bc912feb8f2bfe38ee47ba
red-kite <38669987+red-kite@users.noreply.github.com>
2024-04-09 14:43:27 +0200

    Show link on hover (#634)

--- ReText/editor.py
+++ ReText/editor.py
@@ -52,6 +52,9 @@ colors = {
 	'restDirectives':       {'light': '#800080', 'dark': '#d070d0'},
 	'restRoles':            {'light': '#800000', 'dark': '#d07070'},
 	'whitespaceOnEnd':      {'light': '#80e1e1a5', 'dark': '#8096966e'},
+	# Preview
+	'urlPopupArea':         {'light': '#fafafafa', 'dark': '#fa323232'},
+	'urlPopupBorder':       {'light': '#64323232', 'dark': '#64fafafa'},
 }
 
 colorValues = {}
