39939a4aad350f0301e697230ba353cdcbf27c80
Dmitry Shachnev <mitya57@gmail.com>
2025-10-18 22:01:07 +0300

    Fix ruff error

--- ReText/syncscroll.py
+++ ReText/syncscroll.py
@@ -14,8 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from bisect import bisect_left
 import time
+from bisect import bisect_left
 
 from PyQt6.QtCore import QPoint
 
